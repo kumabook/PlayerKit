0351f85802947fff9bafb73c75d51dc8bd1b046e
Hiroki Kumamoto <kumabook@live.jp>
2015-09-12 22:33:34 +0900

    Create MiniPlayerView ui parts programatically intead of MiniPlayerView.xib

--- Source/MiniPlayerView.swift
+++ Source/MiniPlayerView.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import SnapKit
 
 public class MiniPlayerView: UIView {
-    @IBOutlet public weak var durationLabel:  UILabel!
-    @IBOutlet public weak var titleLabel:     UILabel!
-    @IBOutlet public weak var playButton:     UIButton!
-    @IBOutlet public weak var previousButton: UIButton!
-    @IBOutlet public weak var nextButton:     UIButton!
-    public var delegate:       MiniPlayerViewDelegate?
+    public var durationLabel:  UILabel!
+    public var titleLabel:     UILabel!
+    public var playButton:     UIButton!
+    public var previousButton: UIButton!
+    public var nextButton:     UIButton!
+    let paddingSide:   CGFloat =  8.0
+    let buttonSize:    CGFloat = 40.0
+    let buttonPadding: CGFloat = 30.0
+    public var delegate: MiniPlayerViewDelegate?
     private var _state: PlayerState = .Pause
     public var state: PlayerState {
         get { return _state }
@@ -28,16 +32,71 @@ public class MiniPlayerView: UIView {
         super.init(coder: aDecoder)
         baseInit()
     }
-    
+
     func baseInit() {
         let bundle = NSBundle(identifier: "io.kumabook.PlayerKit")
-        let view = bundle!.loadNibNamed("MiniPlayerView", owner:self, options:nil)[0] as! UIView
-        view.frame = self.bounds;
-        view.autoresizingMask = UIViewAutoresizing.FlexibleWidth|UIViewAutoresizing.FlexibleHeight;
-        self.addSubview(view)
-        self.playButton.addTarget(    self, action: "playButtonTapped",     forControlEvents: UIControlEvents.TouchUpInside)
-        self.previousButton.addTarget(self, action: "previousButtonTapped", forControlEvents: UIControlEvents.TouchUpInside)
-        self.nextButton.addTarget(    self, action: "nextButtonTapped",     forControlEvents: UIControlEvents.TouchUpInside)
+        backgroundColor = UIColor.darkGrayColor()
+        durationLabel   = UILabel(frame: CGRectMake( 0, 0,  64, 20))
+        titleLabel      = UILabel(frame: CGRectMake( 0, 0, 120, 20))
+        playButton      = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+        previousButton  = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+        nextButton      = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+
+        addSubview(durationLabel)
+        addSubview(titleLabel)
+        addSubview(playButton)
+        addSubview(previousButton)
+        addSubview(nextButton)
+
+        durationLabel.font      = UIFont.boldSystemFontOfSize(15.0)
+        durationLabel.textColor = UIColor.whiteColor()
+        durationLabel.snp_makeConstraints { make in
+            make.right.equalTo(self.snp_right).offset(-self.paddingSide)
+            make.bottom.equalTo(self.snp_bottom).offset(-self.paddingSide)
+        }
+
+        titleLabel.font          = UIFont.boldSystemFontOfSize(15.0)
+        titleLabel.textColor     = UIColor.whiteColor()
+        titleLabel.numberOfLines = 1
+        titleLabel.lineBreakMode = NSLineBreakMode.ByTruncatingTail
+        titleLabel.snp_makeConstraints { make in
+            make.left.equalTo(self.nextButton.snp_right).offset(self.paddingSide)
+            make.right.equalTo(self.snp_right).offset(-self.paddingSide)
+            make.top.equalTo(self.snp_top).offset(self.paddingSide)
+        }
+
+        playButton.contentMode = UIViewContentMode.ScaleToFill
+        playButton.tintColor   = UIColor.whiteColor()
+        playButton.snp_makeConstraints { make in
+            make.centerX.equalTo(self.snp_centerX)
+            make.centerY.equalTo(self.snp_centerY)
+            make.width.equalTo(self.buttonSize * 3/5)
+            make.height.equalTo(self.buttonSize * 3/5)
+        }
+
+        previousButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        previousButton.setImage(UIImage(named: "previous", inBundle: bundle, compatibleWithTraitCollection: nil), forState: .allZeros)
+        previousButton.tintColor   = UIColor.whiteColor()
+        previousButton.snp_makeConstraints { make in
+            make.centerY.equalTo(self.snp_centerY)
+            make.right.equalTo(self.playButton.snp_left).offset(-self.buttonPadding)
+            make.width.equalTo(self.buttonSize)
+            make.height.equalTo(self.buttonSize)
+        }
+
+        nextButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        nextButton.setImage(UIImage(named: "next", inBundle: bundle, compatibleWithTraitCollection: nil), forState: .allZeros)
+        nextButton.tintColor   = UIColor.whiteColor()
+        nextButton.snp_makeConstraints { make in
+            make.centerY.equalTo(self.snp_centerY)
+            make.left.equalTo(self.playButton.snp_right).offset(self.buttonPadding)
+            make.width.equalTo(self.buttonSize)
+            make.height.equalTo(self.buttonSize)
+        }
+
+        playButton.addTarget(    self, action: "playButtonTapped",     forControlEvents: UIControlEvents.TouchUpInside)
+        previousButton.addTarget(self, action: "previousButtonTapped", forControlEvents: UIControlEvents.TouchUpInside)
+        nextButton.addTarget(    self, action: "nextButtonTapped",     forControlEvents: UIControlEvents.TouchUpInside)
 
         playButton.setImage(UIImage(named: "play", inBundle: bundle, compatibleWithTraitCollection: nil), forState: UIControlState.Normal)
         self._state = .Pause
